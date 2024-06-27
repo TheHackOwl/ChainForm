46a5b844563becedd77b4711d5612024e2f22479
Luke <li17688618461@gmail.com>
2024-06-27 21:49:05 +0800

    feat: add rewardoption

--- components/sidebar/token-balance.tsx
+++ components/sidebar/token-balance.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { formatEther } from "viem";
 import { useReadContract, useAccount } from "wagmi";
 
+import { getFormatNumberRegex } from "@/lib/utils";
 import { MY_TOKNE_ABI, MY_TOKNE_ADDRESS } from "@/constants/contract/myToken";
 
 interface TokenBalanceProps {}
+const decimalPlaces = 9;
 
 export const TokenBalance: React.FC<TokenBalanceProps> = () => {
   const { address } = useAccount();
+  const [balance, setBalance] = useState<string>("0");
+
   const { data, isLoading } = useReadContract({
     abi: MY_TOKNE_ABI,
     address: MY_TOKNE_ADDRESS,
@@ -17,10 +21,25 @@ export const TokenBalance: React.FC<TokenBalanceProps> = () => {
     args: [address!],
   });
 
+  const regex = useMemo(() => {
+    return getFormatNumberRegex(decimalPlaces);
+  }, [decimalPlaces]);
+
+  useEffect(() => {
+    if (data == undefined) return;
+    const res = formatEther(data!).match(regex);
+
+    if (res) {
+      setBalance(res[1]);
+    } else {
+      setBalance(formatEther(data!));
+    }
+  }, [data]);
+
   return (
     <div className="mt-4">
       <div className="text-slate-100">My token</div>
-      {!isLoading && <div className="text-2xl">{formatEther(data!)}</div>}
+      {!isLoading && <div className="text-2xl">{balance}</div>}
     </div>
   );
 };
